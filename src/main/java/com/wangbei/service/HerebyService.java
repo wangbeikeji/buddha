2c23a69ae9074196eb02873d09d15a9b24c31b27
493671072@qq.com <493671072@qq.com>
2017-07-15 16:10:11 +0800

    none

--- src/main/java/com/wangbei/service/HerebyService.java
+++ src/main/java/com/wangbei/service/HerebyService.java
@@ -1,11 +1,14 @@
 package com.wangbei.service;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.wangbei.dao.HerebyDao;
 import com.wangbei.entity.Hereby;
+import com.wangbei.exception.ServiceException;
 
 /**
  * @author yuyidi 2017-07-14 11:33:00
@@ -20,8 +23,13 @@ public class HerebyService {
 
     @Transactional
     public Hereby addHereby(Integer user, Integer joss) {
-        Hereby hereby = new Hereby(user, joss);
-        return herebyDao.create(hereby);
+    	Hereby hereby = new Hereby(user, joss);
+		hereby.setCreateTime(new Date());
+		try {
+			return herebyDao.create(hereby);
+		} catch (Exception ex) {
+			throw new ServiceException(ServiceException.USER_ADDHEREBY_DUPLICATE_EXCEPTION);
+		}
     }
 
     @Transactional
