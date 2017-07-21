6cc51547d160d2b6ab36aaaca8abfd1cfa82e656
493671072@qq.com <493671072@qq.com>
2017-07-21 17:49:40 +0800

    修改摇签随机的算法

--- src/main/java/com/wangbei/service/RuneService.java
+++ src/main/java/com/wangbei/service/RuneService.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import com.wangbei.entity.Beg;
-import com.wangbei.entity.Divination;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.wangbei.dao.RuneDao;
+import com.wangbei.entity.Beg;
 import com.wangbei.entity.Rune;
 
 /**
