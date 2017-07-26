5f3278371207d04d4547f4da451cf3aa42ab2e77
493671072@qq.com <493671072@qq.com>
2017-07-26 18:58:00 +0800

    none

--- src/main/java/com/wangbei/service/BannerService.java
+++ src/main/java/com/wangbei/service/BannerService.java
@@ -38,7 +38,7 @@ public class BannerService {
 			if(imgIndex > 0 && titleIndex > 0){
 				link = link.substring(imgIndex + 10, titleIndex - 2);
 				link = link.substring(link.indexOf("buddha") + 6);
-				banner.setLink(link);
+				banner.setLink("http:"+link);
 			}
 		}
 		return bannerDao.createBanner(banner);
@@ -55,7 +55,7 @@ public class BannerService {
 			if(imgIndex > 0 && titleIndex > 0){
 				link = link.substring(imgIndex + 10, titleIndex - 2);
 				link = link.substring(link.indexOf("buddha") + 6);
-				banner.setLink(link);
+				banner.setLink("http:"+link);
 			}
 		}
 		return bannerDao.updateBanner(banner);
